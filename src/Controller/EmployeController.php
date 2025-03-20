6c281dafdcb66c30e6ffffd5f9aab5a5856d8665
Rachel Marquant <marquantrachel@gmail.com>
2025-03-20 11:38:24 +0100

    Ajout d'un formulaire d'une entreprise fonctionnel

--- src/Controller/EmployeController.php
+++ src/Controller/EmployeController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Employe;
+use App\Form\EmployeType;
 use App\Repository\EmployeRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -17,6 +19,17 @@ final class EmployeController extends AbstractController
         return $this->render('employe/index.html.twig', [
             'employes' => $employes,
         ]);
+    }
+
+    // on le defini avant la fonction show car l'ordi va d'abbord chercher entreprise/id avant entreprise/new se qui cree une erreur
+    #[Route('/employe/new', name: 'new_employe')] 
+    public function new(Request $request): Response{
+        $employe = new Employe();
+        // on cree une nouvelle Employe 
+        $form = $this->createForm(EmployeType::class, $employe); 
+
+        return $this->render('employe/new.html.twig',
+                ['formAddEmploye' => $form]);
     }
 
     #[Route('/employe/{id}', name: 'show_employe')]
--- src/Controller/EntrepriseController.php
+++ src/Controller/EntrepriseController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Entreprise;
+use App\Form\EntrepriseType;
 use App\Repository\EntrepriseRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -19,10 +21,33 @@ final class EntrepriseController extends AbstractController
             'entreprises' => $entreprises
         ]);
     }
+
+    // on le defini avant la fonction show car l'ordi va d'abbord chercher entreprise/id avant entreprise/new se qui cree une erreur
+    #[Route('/entreprise/new', name: 'new_entreprise')] 
+    public function new(Request $request, EntityManagerInterface $entityManager): Response{
+        $entreprise = new Entreprise();
+        // on cree une nouvelle entreprise 
+        $form = $this->createForm(EntrepriseType::class, $entreprise); 
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() and $form->isValid()){
+            $entreprise = $form->getData();
+
+            $entityManager->persist($entreprise); // c'est la prepare en PDO
+            $entityManager->flush(); // c'est l'execute en PDO
+
+            return $this->redirectToRoute('app_entreprise');
+        }
+
+        return $this->render('entreprise/new.html.twig',
+                ['formAddEntreprise' => $form]);
+    }
+
     // {id} est la variable que l'on recupere dans l'URL
     #[Route('/entreprise/{id}', name: 'show_entreprise')] 
     public function show(Entreprise $entreprise): Response{
         return $this->render('entreprise/show.html.twig',
                 ['entreprise' => $entreprise]);
     }
+    
 }
